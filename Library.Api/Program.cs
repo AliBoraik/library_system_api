d441b2c286096c0f493e3215eee3820c471d3c32
AliBoraik <ali99aqel@gmail.com>
2024-11-23 04:39:40 +0300

    test docker compose action on github

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddAppl(builder.Configuration);
+builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddSwaggerConfiguration();
 // Redis OutputCache
 builder.Services.RedisOutputCache(builder.Configuration);
